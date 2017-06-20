77698233678c01e5cfef4f484b306b19d0b5de49
sarikachaudhari <sarikach244@gmail.com>
2017-06-20 17:18:08 +0530

    created url.py file

--- Practices/Practices/urls.py
+++ Practices/Practices/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+   url(r'^admin/', admin.site.urls), 
+   url(r'^get/emp/', get_employee),
+   url(r'^add/emp/', add_employee),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
